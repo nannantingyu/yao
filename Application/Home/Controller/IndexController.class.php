9045bc075ae844a8900ef23603e4590b170897f8
yangmingming <yangmingming@91jin.com>
2016-03-29 09:11:56 +0800

    鑫尧Love

---
+++ Application/Home/Controller/IndexController.class.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Home\Controller;
+use Think\Controller;
+
+class IndexController extends Controller {
+    public function index(){
+        $this->display();
+    }
+
+    public function cart(){
+        $this->display();
+    }
+
+    public function shop(){
+        $this->display();
+    }
+
+    public function checkout(){
+        $this->display();
+    }
+
+    public function detail(){
+        $this->display();
+    }
+}
