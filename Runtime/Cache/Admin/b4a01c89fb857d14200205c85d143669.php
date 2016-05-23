8249fcd20a610d3b0070b0a7faad1fbca2fba3aa
鑫尧 <xinyao@xy.com>
2016-05-23 21:21:04 +0800

    finnal

---
+++ Runtime/Cache/Admin/b4a01c89fb857d14200205c85d143669.php
@@ -0,0 +1,344 @@
+<?php if (!defined('THINK_PATH')) exit();?><!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>ZCStore后台管理</title>
+    <link href="/Public/favicon.ico" type="image/x-icon" rel="shortcut icon">
+    <link rel="stylesheet" type="text/css" href="/Public/Admin/css/base.css" media="all">
+    <link rel="stylesheet" type="text/css" href="/Public/Admin/css/common.css" media="all">
+    <link rel="stylesheet" type="text/css" href="/Public/Admin/css/module.css">
+    <link rel="stylesheet" type="text/css" href="/Public/Admin/css/style.css" media="all">
+	<link rel="stylesheet" type="text/css" href="/Public/Admin/css/<?php echo (C("COLOR_STYLE")); ?>.css" media="all">
+     <!--[if lt IE 9]>
+    <script type="text/javascript" src="/Public/static/jquery-1.10.2.min.js"></script>
+    <![endif]--><!--[if gte IE 9]><!-->
+    <script type="text/javascript" src="/Public/static/jquery-2.0.3.min.js"></script>
+    <script type="text/javascript" src="/Public/Admin/js/jquery.mousewheel.js"></script>
+    <script type="text/javascript" src="/Public/Admin/js/webuploader/webuploader.js"></script>
+    <script type="text/javascript" src="/Public/Admin/js/uploader.js"></script>
+    <!--<![endif]-->
+    
+	<style>
+		.dragsort {
+		    width:400px;
+		}
+	</style>
+
+</head>
+<body>
+    <!-- 头部 -->
+    <div class="header">
+        <!-- Logo -->
+        <span class="logo"></span>
+        <!-- /Logo -->
+
+        <!-- 主导航 -->
+        <ul class="main-nav">
+            <?php if(is_array($__MENU__["main"])): $i = 0; $__LIST__ = $__MENU__["main"];if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$menu): $mod = ($i % 2 );++$i;?><li class="<?php echo ((isset($menu["class"]) && ($menu["class"] !== ""))?($menu["class"]):''); ?>"><a href="<?php echo (u($menu["url"])); ?>"><?php echo ($menu["title"]); ?></a></li><?php endforeach; endif; else: echo "" ;endif; ?>
+        </ul>
+        <!-- /主导航 -->
+
+        <!-- 用户栏 -->
+        <div class="user-bar">
+            <a href="javascript:;" class="user-entrance"><i class="icon-user"></i></a>
+            <ul class="nav-list user-menu hidden">
+                <li class="manager">你好，<em title="<?php echo session('user_auth.username');?>"><?php echo session('user_auth.username');?></em></li>
+                <li><a href="<?php echo U('User/updatePassword');?>">修改密码</a></li>
+                <li><a href="<?php echo U('User/updateNickname');?>">修改昵称</a></li>
+                <li><a href="<?php echo U('Public/logout');?>">退出</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- /头部 -->
+
+    <!-- 边栏 -->
+    <div class="sidebar">
+        <!-- 子导航 -->
+        
+	<div id="subnav" class="subnav">
+    <?php if(!empty($_extra_menu)): ?>
+        <?php echo extra_menu($_extra_menu,$__MENU__); endif; ?>
+    <?php if(is_array($__MENU__["child"])): $i = 0; $__LIST__ = $__MENU__["child"];if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$sub_menu): $mod = ($i % 2 );++$i;?><!-- 子导航 -->
+        <?php if(!empty($sub_menu)): if(!empty($key)): ?><h3><i class="icon icon-unfold"></i><?php echo ($key); ?></h3><?php endif; ?>
+            <ul class="side-sub-menu">
+                <?php if(is_array($sub_menu)): $i = 0; $__LIST__ = $sub_menu;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$menu): $mod = ($i % 2 );++$i;?><li>
+                        <a class="item" href="<?php echo (u($menu["url"])); ?>"><?php echo ($menu["title"]); ?></a>
+                    </li><?php endforeach; endif; else: echo "" ;endif; ?>
+            </ul><?php endif; ?>
+        <!-- /子导航 --><?php endforeach; endif; else: echo "" ;endif; ?>
+ <h3>
+ 	<i class="icon <?php if(!in_array((ACTION_NAME), explode(',',"mydocument,draftbox,examine"))): ?>icon-fold<?php endif; ?>"></i>
+ 	广告管理
+ </h3>
+ 	<ul class="side-sub-menu <?php if(!in_array((ACTION_NAME), explode(',',"index"))): ?>subnav-on<?php endif; ?>">
+        <li <?php if((ACTION_NAME) == "index"): ?>class="current"<?php endif; ?>><a class="item" href="<?php echo U('banner/index');?>">全部广告</a></li>
+ 	</ul>
+
+    <?php if(is_array($nodes)): $i = 0; $__LIST__ = $nodes;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$sub_menu): $mod = ($i % 2 );++$i;?><!-- 子导航 -->
+        <?php if(!empty($sub_menu)): ?><h3>
+            	<i class="icon <?php if(($sub_menu['current']) != "1"): ?>icon-fold<?php endif; ?>"></i>
+            	<?php if(($sub_menu['allow_publish']) > "0"): ?><a class="item" href="<?php echo (u($sub_menu["url"])); ?>"><?php echo ($sub_menu["title"]); ?></a><?php else: echo ($sub_menu["title"]); endif; ?>
+            </h3>
+            <ul class="side-sub-menu <?php if(($sub_menu["current"]) != "1"): ?>subnav-off<?php endif; ?>">
+                <?php if(is_array($sub_menu['_child'])): $i = 0; $__LIST__ = $sub_menu['_child'];if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$menu): $mod = ($i % 2 );++$i;?><li <?php if($menu['id'] == $cate_id or $menu['current'] == 1): ?>class="current"<?php endif; ?>>
+                        <?php if(($menu['allow_publish']) > "0"): ?><a class="item" href="<?php echo (u($menu["url"])); ?>"><?php echo ($menu["title"]); ?></a><?php else: ?><a class="item" href="javascript:void(0);"><?php echo ($menu["title"]); ?></a><?php endif; ?>
+
+                        <!-- 一级子菜单 -->
+                        <?php if(isset($menu['_child'])): ?><ul class="subitem">
+                        	<?php if(is_array($menu['_child'])): foreach($menu['_child'] as $key=>$three_menu): ?><li>
+                                <?php if(($three_menu['allow_publish']) > "0"): ?><a class="item" href="<?php echo (u($three_menu["url"])); ?>"><?php echo ($three_menu["title"]); ?></a><?php else: ?><a class="item" href="javascript:void(0);"><?php echo ($three_menu["title"]); ?></a><?php endif; ?>
+                                <!-- 二级子菜单 -->
+                                <?php if(isset($three_menu['_child'])): ?><ul class="subitem">
+                                	<?php if(is_array($three_menu['_child'])): foreach($three_menu['_child'] as $key=>$four_menu): ?><li>
+                                        <?php if(($four_menu['allow_publish']) > "0"): ?><a class="item" href="<?php echo U('index','cate_id='.$four_menu['id']);?>"><?php echo ($four_menu["title"]); ?></a><?php else: ?><a class="item" href="javascript:void(0);"><?php echo ($four_menu["title"]); ?></a><?php endif; ?>
+                                        <!-- 三级子菜单 -->
+                                        <?php if(isset($four_menu['_child'])): ?><ul class="subitem">
+                                        	<?php if(is_array($four_menu['_child'])): $i = 0; $__LIST__ = $four_menu['_child'];if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$five_menu): $mod = ($i % 2 );++$i;?><li>
+                                            	<?php if(($five_menu['allow_publish']) > "0"): ?><a class="item" href="<?php echo U('index','cate_id='.$five_menu['id']);?>"><?php echo ($five_menu["title"]); ?></a><?php else: ?><a class="item" href="javascript:void(0);"><?php echo ($five_menu["title"]); ?></a><?php endif; ?>
+                                            </li><?php endforeach; endif; else: echo "" ;endif; ?>
+                                        </ul><?php endif; ?>
+                                        <!-- end 三级子菜单 -->
+                                    </li><?php endforeach; endif; ?>
+                                </ul><?php endif; ?>
+                                <!-- end 二级子菜单 -->
+                            </li><?php endforeach; endif; ?>
+                        </ul><?php endif; ?>
+                        <!-- end 一级子菜单 -->
+                    </li><?php endforeach; endif; else: echo "" ;endif; ?>
+            </ul><?php endif; ?>
+        <!-- /子导航 --><?php endforeach; endif; else: echo "" ;endif; ?>
+    <!-- 回收站 -->
+	<?php if(($show_recycle) == "1"): ?><h3>
+        <em class="recycle"></em>
+        <a href="<?php echo U('article/recycle');?>">回收站</a>
+    </h3><?php endif; ?>
+</div>
+<script>
+    $(function(){
+        $(".side-sub-menu li").hover(function(){
+            $(this).addClass("hover");
+        },function(){
+            $(this).removeClass("hover");
+        });
+    })
+</script>
+
+
+        <!-- /子导航 -->
+    </div>
+    <!-- /边栏 -->
+
+    <!-- 内容区 -->
+    <div id="main-content">
+        <div id="top-alert" class="fixed alert alert-error" style="display: none;">
+            <button class="close fixed" style="margin-top: 4px;">&times;</button>
+            <div class="alert-content">这是内容</div>
+        </div>
+        <div id="main" class="main">
+            
+            <!-- nav -->
+            <?php if(!empty($_show_nav)): ?><div class="breadcrumb">
+                <span>您的位置:</span>
+                <?php $i = '1'; ?>
+                <?php if(is_array($_nav)): foreach($_nav as $k=>$v): if($i == count($_nav)): ?><span><?php echo ($v); ?></span>
+                    <?php else: ?>
+                    <span><a href="<?php echo ($k); ?>"><?php echo ($v); ?></a>&gt;</span><?php endif; ?>
+                    <?php $i = $i+1; endforeach; endif; ?>
+            </div><?php endif; ?>
+            <!-- nav -->
+            
+
+            
+
+	<script type="text/javascript" src="/Public/static/uploadify/jquery.uploadify.min.js"></script>
+	<!-- 标题栏 -->
+	<div class="main-title cf">
+		<h2><?php if(isset($banner)): ?>编辑<?php else: ?>新增<?php endif; ?>广告</h2>
+	</div>
+
+		<!-- 修改密码表单 -->
+	<form method="post" class="form-horizontal">
+		<div class="form-item cf">
+			<label class="item-label">广告名称</label>
+			<div class="controls">
+				<input type="text" value="<?php echo ($banner["title"]); ?>" name="title" class="text input-large">
+			</div>
+		</div>
+
+		<div class="form-item cf">
+			<label class="item-label">广告链接</label>
+			<div class="controls">
+				<input type="text" value="<?php echo ($banner["link"]); ?>" name="link" class="text input-large">
+			</div>
+		</div>
+
+		<div class="form-item cf">
+			<label class="item-label">广告图片</label>
+			<div class="controls">
+				<input type="file" id="upload_file_banner_img">
+				<input type="hidden" name="image" value="<?php echo ($banner['image']); ?>"/>
+				<div class="upload-img-box">
+					<?php if(isset($banner['image'])): ?><div class="upload-pre-item"><img id="banner_img" src="<?php echo ($banner['image']); ?>"/></div><?php endif; ?>
+				</div>
+			</div>
+			<script type="text/javascript">
+				//上传图片
+				/* 初始化上传插件 */
+				$("#upload_file_banner_img").uploadify({
+					"height"          : 30,
+					"swf"             : "/Public/static/uploadify/uploadify.swf",
+					"fileObjName"     : "download",
+					"buttonText"      : "上传图片",
+					"uploader"        : "<?php echo U('File/uploadBanner',array('session_id'=>session_id()));?>",
+					"width"           : 120,
+					'removeTimeout'	  : 1,
+					"onUploadSuccess" : uploadFilegoods_img,
+				'onFallback' : function() {
+					alert('未检测到兼容版本的Flash.');
+				}
+				});
+				function uploadFilegoods_img(file, data){
+					var data = $.parseJSON(data);
+					if(data.status){
+						var name = "image";
+						$("input[name="+name+"]").val(data.path);
+						$("input[name="+name+"]").parent().find('.upload-img-box').html(
+								"<div class=\"upload-pre-file\"><span class=\"upload_icon_all\"></span>" + data.info + "</div>"
+								+ "<div class=\"upload-pre-item\"><img id=\"banner_img\" src=\"" + data.path + "\"/></div>"
+						);
+					} else {
+						updateAlert(data.info);
+						setTimeout(function(){
+							$('#top-alert').find('button').click();
+							$(that).removeClass('disabled').prop('disabled',false);
+						},1500);
+					}
+				}
+			</script>
+		</div>
+
+		<div class="form-item cf">
+			<label class="item-label">添加时间</label>
+			<div class="controls">
+				<input type="text" value="<?php echo ((isset($banner["created_time"]) && ($banner["created_time"] !== ""))?($banner["created_time"]):date('Y-m-d H:i:s')); ?>" name="created_time" class="text input-large">
+			</div>
+		</div>
+
+		<div class="form-item cf">
+			<label class="item-label">状态</label>
+			<div class="controls">
+				<select name="state">
+					<option value="1" <?php if(($banner["state"]) == "1"): ?>selected<?php endif; ?>>发布</option>
+					<option value="0" <?php if(($banner["state"]) == "0"): ?>selected<?php endif; ?>>下线</option>
+				</select>
+			</div>
+		</div>
+
+		<input type="hidden" name="id" value="<?php echo ($banner["id"]); ?>">
+		<button type="submit" class="btn submit-btn ajax-post" target-form="form-horizontal">确 定</button>
+		<button class="btn btn-return" onclick="javascript:history.back(-1);return false;">返 回</button>
+	</form>
+
+        </div>
+        <div class="cont-ft">
+            <div class="copyright">
+                <div class="fl">感谢使用<a href="http://www.onethink.cn" target="_blank">OneThink</a>管理平台</div>
+                <div class="fr">V<?php echo (ONETHINK_VERSION); ?></div>
+            </div>
+        </div>
+    </div>
+    <!-- /内容区 -->
+    <script type="text/javascript">
+    (function(){
+        var ThinkPHP = window.Think = {
+            "ROOT"   : "", //当前网站地址
+            "APP"    : "", //当前项目地址
+            "PUBLIC" : "/Public", //项目公共目录地址
+            "DEEP"   : "<?php echo C('URL_PATHINFO_DEPR');?>", //PATHINFO分割符
+            "MODEL"  : ["<?php echo C('URL_MODEL');?>", "<?php echo C('URL_CASE_INSENSITIVE');?>", "<?php echo C('URL_HTML_SUFFIX');?>"],
+            "VAR"    : ["<?php echo C('VAR_MODULE');?>", "<?php echo C('VAR_CONTROLLER');?>", "<?php echo C('VAR_ACTION');?>"]
+        }
+    })();
+    </script>
+    <script type="text/javascript" src="/Public/static/think.js"></script>
+    <script type="text/javascript" src="/Public/Admin/js/common.js"></script>
+    <script type="text/javascript">
+        +function(){
+            var $window = $(window), $subnav = $("#subnav"), url;
+            $window.resize(function(){
+                $("#main").css("min-height", $window.height() - 130);
+            }).resize();
+
+            /* 左边菜单高亮 */
+            url = window.location.pathname + window.location.search;
+            url = url.replace(/(\/(p)\/\d+)|(&p=\d+)|(\/(id)\/\d+)|(&id=\d+)|(\/(group)\/\d+)|(&group=\d+)/, "");
+            $subnav.find("a[href='" + url + "']").parent().addClass("current");
+
+            /* 左边菜单显示收起 */
+            $("#subnav").on("click", "h3", function(){
+                var $this = $(this);
+                $this.find(".icon").toggleClass("icon-fold");
+                $this.next().slideToggle("fast").siblings(".side-sub-menu:visible").
+                      prev("h3").find("i").addClass("icon-fold").end().end().hide();
+            });
+
+            $("#subnav h3 a").click(function(e){e.stopPropagation()});
+
+            /* 头部管理员菜单 */
+            $(".user-bar").mouseenter(function(){
+                var userMenu = $(this).children(".user-menu ");
+                userMenu.removeClass("hidden");
+                clearTimeout(userMenu.data("timeout"));
+            }).mouseleave(function(){
+                var userMenu = $(this).children(".user-menu");
+                userMenu.data("timeout") && clearTimeout(userMenu.data("timeout"));
+                userMenu.data("timeout", setTimeout(function(){userMenu.addClass("hidden")}, 100));
+            });
+
+	        /* 表单获取焦点变色 */
+	        $("form").on("focus", "input", function(){
+		        $(this).addClass('focus');
+	        }).on("blur","input",function(){
+				        $(this).removeClass('focus');
+			        });
+		    $("form").on("focus", "textarea", function(){
+			    $(this).closest('label').addClass('focus');
+		    }).on("blur","textarea",function(){
+			    $(this).closest('label').removeClass('focus');
+		    });
+
+            // 导航栏超出窗口高度后的模拟滚动条
+            var sHeight = $(".sidebar").height();
+            var subHeight  = $(".subnav").height();
+            var diff = subHeight - sHeight; //250
+            var sub = $(".subnav");
+            if(diff > 0){
+                $(window).mousewheel(function(event, delta){
+                    if(delta>0){
+                        if(parseInt(sub.css('marginTop'))>-10){
+                            sub.css('marginTop','0px');
+                        }else{
+                            sub.css('marginTop','+='+10);
+                        }
+                    }else{
+                        if(parseInt(sub.css('marginTop'))<'-'+(diff-10)){
+                            sub.css('marginTop','-'+(diff-10));
+                        }else{
+                            sub.css('marginTop','-='+10);
+                        }
+                    }
+                });
+            }
+        }();
+    </script>
+    
+	<?php if(isset($data)): ?><script type="text/javascript" src="/Public/static/jquery.dragsort-0.5.1.min.js"></script><?php endif; ?>
+	<script type="text/javascript">
+		$(function(){
+			//导航高亮
+			highlight_subnav('<?php echo U('banner/index');?>');
+		})
+	</script>
+
+</body>
+</html>
--- Runtime/Cache/Home/d74389d7fdd319385f06c12ee6d02938.php
+++ Runtime/Cache/Home/d74389d7fdd319385f06c12ee6d02938.php
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>我的ZC---个人中心</title>
-<link href="__CSS__/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="__CSS__/style.css">
-<script src="__JS__/jquery-2.1.1.js"></script>
-<script src="__JS__/bootstrap.min.js"></script>
+<link href="/Public/Home/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="/Public/Home/css/style.css">
+<script src="/Public/Home/js/jquery-2.1.1.js"></script>
+<script src="/Public/Home/js/bootstrap.min.js"></script>
 <style>
     .mytb td img{
         width: 30px !important;
@@ -25,21 +25,19 @@
         </div>
         <div id="user-div" class="pull-right">
             <ul class="nav navbar-nav">
-                <php>$uname = session('uname');</php>
-                <if condition="is_null($uname)">
-                    <li><a href="/home/user/account.html"><span class="glyphicon glyphicon-envelope"></span> 登录</a></li>
-                    <else />
+                <?php $uname = session('uname'); ?>
+                <?php if(is_null($uname)): ?><li><a href="/home/user/account.html"><span class="glyphicon glyphicon-envelope"></span> 登录</a></li>
+                    <?php else: ?>
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            欢迎你, {$uname} <b class="caret"></b>
+                            欢迎你, <?php echo ($uname); ?> <b class="caret"></b>
                         </a>
                         <ul class="dropdown-menu">
                             <li><a href="/home/user/personal">我的账户</a></li>
                             <li><a href="/home/user/logout.html">退出登录</a></li>
                         </ul>
                     </li>
-                    <li><a href="/home/user/myorder">我的订单</a></li>
-                </if>
+                    <li><a href="/home/user/myorder">我的订单</a></li><?php endif; ?>
                 <li><a href="/home/index/contact.html"><span class="glyphicon glyphicon-envelope"></span> 联系我们</a></li>
             </ul>
         </div>
@@ -49,7 +47,7 @@
 <header class="container">
     <div class="row">
         <div class="col-md-4">
-            <div id="logo"><a href="/"><img src="__IMG__/logo.png" /></a></div>
+            <div id="logo"><a href="/"><img src="/Public/Home/images/logo.png" /></a></div>
         </div>
         <div class="col-md-4">
             <form class="form-search">
@@ -58,7 +56,7 @@
             </form>
         </div>
         <div class="col-md-4">
-            <div id="cart"><a class="btn btn-1" href="/home/goods/cart.html"><span class="glyphicon glyphicon-shopping-cart"></span>购物车 : <span id="cart-count"><php>echo count(session('cart'));</php></span> 件</a></div>
+            <div id="cart"><a class="btn btn-1" href="/home/goods/cart.html"><span class="glyphicon glyphicon-shopping-cart"></span>购物车 : <span id="cart-count"><?php echo count(session('cart')); ?></span> 件</a></div>
         </div>
     </div>
 </header>
@@ -69,7 +67,7 @@
    </div>
    <div class="panel-body">
         <ul class="list-group">
-            <li class="list-group-item">收货人信息&nbsp;&nbsp;{$address}&nbsp;&nbsp;<a href="/home/user/personal.html#section-7">[修改]</a>|<a>[新增并使用]</a></li>
+            <li class="list-group-item">收货人信息&nbsp;&nbsp;<?php echo ($address); ?>&nbsp;&nbsp;<a href="/home/user/personal.html#section-7">[修改]</a>|<a>[新增并使用]</a></li>
             <li class="list-group-item">支付及配送方式&nbsp;&nbsp;
                 您选择使用：&nbsp;支付宝&nbsp;<a href="#">[修改]</a>
             </li>
@@ -83,21 +81,19 @@
                     <th>促销价</th>
                     <th width="16%">数量</th>
                 </tr>
-                <volist name="orders" id="goods">
-                    <tr>
-                        <td align="center"><img src="{:getImgsrc($goods['goods_img'])}" alt="{$goods.goods_name}" width="30"></td>
-                        <td>{$goods.goods_name}</td>
-                        <td>{$goods.market_price}</td>
-                        <td>{$goods.goods_price}</td>
-                        <td>{$goods.goods_number}</td>
-                    </tr>
-                </volist>
-                <tr><td colspan="4"></td><td>商品总计：{$gsprice}元<br><br><button onclick="checkAll(this)" class="btn btn-danger">付款</button></td></tr>
+                <?php if(is_array($orders)): $i = 0; $__LIST__ = $orders;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$goods): $mod = ($i % 2 );++$i;?><tr>
+                        <td align="center"><img src="<?php echo getImgsrc($goods['goods_img']);?>" alt="<?php echo ($goods["goods_name"]); ?>" width="30"></td>
+                        <td><?php echo ($goods["goods_name"]); ?></td>
+                        <td><?php echo ($goods["market_price"]); ?></td>
+                        <td><?php echo ($goods["goods_price"]); ?></td>
+                        <td><?php echo ($goods["goods_number"]); ?></td>
+                    </tr><?php endforeach; endif; else: echo "" ;endif; ?>
+                <tr><td colspan="4"></td><td>商品总计：<?php echo ($gsprice); ?>元<br><br><button onclick="checkAll(this)" class="btn btn-danger">付款</button></td></tr>
             </table>
             </li>
         </ul>
     </div>
-        <input type="hidden" id="order_id" value="{$oid}">
+        <input type="hidden" id="order_id" value="<?php echo ($oid); ?>">
    </div>
 </div>
 </div>
