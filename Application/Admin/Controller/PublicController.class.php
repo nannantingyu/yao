515b872c5bddb9a287f85427cd9d056bcae1af2d
杨明明 <Administrator@yangmingming2.guoxin.bj>
2016-05-26 12:18:44 +0800

    徒徒，吃饭了么

--- Application/Admin/Controller/PublicController.class.php
+++ Application/Admin/Controller/PublicController.class.php
@@ -36,7 +36,7 @@ class PublicController extends \Think\Controller {
                 $Member = D('Member');
                 if($Member->login($uid)){ //登录用户
                     //TODO:跳转到登录前页面
-                    $this->success('登录成功！', U('Goods/index'));
+                    $this->success('登录成功！', U('Index/index'));
                 } else {
                     $this->error($Member->getError());
                 }
